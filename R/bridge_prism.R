bd575b8ed6c9fda983a1b91c6e8a9d3f29ee07f4
Sadatsafavi <msafavi@ead.ubc.ca>
2019-03-07 16:46:15 -0800

    Prism files added

---
+++ R/bridge_prism.R
@@ -0,0 +1,17 @@
+model_run<-function(model_input=NULL)
+{
+  patient_data<-patients[1,]
+  nms<-names(patient_data)
+
+  if(length(model_input)>0)
+  {
+    patient_data[names(model_input)]<-model_input
+  }
+  else
+    stop("Error: no input parameter was submitted")
+
+  res<-predictACCEPT(patient_data)
+  res<-res[-1:-(length(res)-8)]
+
+  return(as.list(res))
+}
